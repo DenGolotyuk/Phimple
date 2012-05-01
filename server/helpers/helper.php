78a020c20a28ac0b3ea8ed4565631b82f2d0bb37
DenGolotyuk <den.golotyuk@gmail.com>
2012-05-01 15:39:19 +0300

    translit

--- server/helpers/helper.php
+++ server/helpers/helper.php
@@ -35,8 +35,11 @@ class helper
 		return $url;
 	}
 	
-	public static function transliterate($text)
+	public static function transliterate($string)
 	{
-		return iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text);
-	} 
+        $rus = array("ё","й","ю","ь","ч","щ","ц","у","к","е","н","г","ш","з","х","ъ","ф","ы","в","а","п","р","о","л","д","ж","э","я","с","м","и","т","б","Ё","Й","Ю","Ч","Ь","Щ","Ц","У","К","Е","Н","Г","Ш","З","Х","Ъ","Ф","Ы","В","А","П","Р","О","Л","Д","Ж","Э","Я","С","М","И","Т","Б");
+        $eng = array("yo","iy","yu","'","ch","sh","c","u","k","e","n","g","sh","z","h","'","f","y","v","a","p","r","o","l","d","j","е","ya","s","m","i","t","b","Yo","Iy","Yu","CH","'","SH","C","U","K","E","N","G","SH","Z","H","'","F","Y","V","A","P","R","O","L","D","J","E","YA","S","M","I","T","B");
+        
+		return str_replace($rus, $eng,  $string);
+    }
 }
