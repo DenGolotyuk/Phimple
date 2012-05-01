c2a5248837f35e6b7bd56106cb105d937a53e754
DenGolotyuk <den.golotyuk@gmail.com>
2012-05-01 15:31:14 +0300

    translit

--- server/helpers/helper.php
+++ server/helpers/helper.php
@@ -35,20 +35,73 @@ class helper
 		return $url;
 	}
 	
-	public static function transliterate($st)
+	public static function transliterate($text, $type = 'en')
 	{
-		$st = strtr($st,  
-			"абвгдежзийклмнопрстуфыэАБВГДЕЖЗИЙКЛМНОПРСТУФЫЭ", 
-			"abvgdegziyklmnoprstufieABVGDEGZIYKLMNOPRSTUFIE" 
-		);
-		
-		$st = strtr($st, array( 
-			'yo'=>"ё",    'h'=>"х",  'ts'=>"ц",  'ch'=>"ч", 'sh'=>"ш",   
-			'shch'=>"o",  ''=>'ъ',   ''=>'ь',    'yu'=>"ю", 'ya'=>"я", 
-			'Yo'=>"Ё",    'H'=>"Х",  'Ts'=>"Ц",  'Ch'=>"Ч", 'Sh'=>"Ш", 
-			'Shch'=>"Щ",  ''=>'Ъ',   ''=>'Ь',    'Yu'=>"Ю", 'Ya'=>"Я", 
-		));
-		
-		return $st;
+		$data=explode(" ",$text); 
+		if(count($data)==''){ 
+		return ''; 
+		} 
+		$alphas=array( 
+		'yii'=>'ы', 
+		'ji'=>'й', 
+		'yo'=>'ё', 
+		'ya'=>'я', 
+		'shc'=>'щ', 
+		'sh'=>'ш', 
+		'ea'=>'я', 
+		'ii'=>'й', 
+		'zh'=>'ж', 
+		'ch'=>'ч', 
+		'iy'=>'ю', 
+		'ts'=>'ц', 
+		'u'=>'у', 
+		'w'=>'в', 
+		'v'=>'в', 
+		'i'=>'и', 
+		'y'=>'у', 
+		'd'=>'д', 
+		't'=>'т', 
+		'b'=>'б', 
+		'p'=>'п', 
+		'n'=>'н', 
+		'f'=>'ф', 
+		'\''=>'ь', 
+		'\''=>'ъ', 
+		'z'=>'з', 
+		'l'=>'л', 
+		'k'=>'к', 
+		's'=>'с', 
+		'm'=>'м', 
+		'r'=>'р', 
+		's'=>'с', 
+		'h'=>'х', 
+		'j'=>'ж', 
+		'g'=>'г', 
+		'_'=>'', 
+		'a'=>'а',
+		'o' => 'о'
+		); 
+		$total=''; 
+		foreach($data as $k=>$v){ 
+		if(preg_match("/^[a-zA-Z]*/",$v)){ 
+		foreach($alphas as $id=>$value){ 
+			if($type=='de'){ 
+				if(strcasecmp($v,$id) AND !preg_match("/->/",$v)){ 
+					$v=str_replace($id,$value,$v); 
+				}elseif(preg_match("/->/",$v)){ 
+					$v=str_replace("->","",$v); 
+				} 
+			}elseif($type='translit'){ 
+				if(strcasecmp($v,$value) AND !preg_match("/->/",$v)){ 
+					$v=str_replace($value,$id,$v); 
+				}elseif(preg_match("/->/",$v)){ 
+					$v=str_replace("->","",$v); 
+				} 
+			} 
+		} 
+		} 
+		$total.=$v." "; 
+		} 
+		return $total;
 	} 
 }
