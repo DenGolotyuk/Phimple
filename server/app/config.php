b65dad3183ac5fe91ad328f5781b2e4024139234
DenGolotyuk <den.golotyuk@gmail.com>
2012-05-05 11:25:10 +0300

    additional configs

--- server/app/config.php
+++ server/app/config.php
@@ -8,14 +8,14 @@ class config
 	
 	public static function init()
 	{
-		if ( defined('ROOT') )
-			self::$data = require ROOT . '/config/app/prod.php';
-		
-		if ( defined('ENV') )
-		{
-			$data = require ROOT . '/config/app/' . ENV . '.php';;
-			self::$data = array_merge(self::$data, $data);
-		}
+		if ( defined('ROOT') ) self::load('prod');
+		if ( defined('ENV') ) self::load(ENV);
+	}
+	
+	public static function load($file)
+	{
+		$data = require ROOT . '/config/app/' . $file . '.php';;
+		self::$data = array_merge(self::$data, $data);
 	}
 	
 	public static function get()
