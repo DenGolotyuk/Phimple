4217e5598a3e32b59dca8e9314706b7431641dee
DenGolotyuk <den.golotyuk@gmail.com>
2012-06-28 17:37:05 +0300

    bettern keys js support

--- client/js/com/key.js
+++ client/js/com/key.js
@@ -1,9 +1,21 @@
 Key = {
 	ctrl: [],
+	esc: null,
+	keys: [],
 	
 	onCtrl: function(key, cb)
 	{
 		Key.ctrl[key] = cb;
+	},
+	
+	onEsc: function(cb)
+	{
+		Key.esc = cb;
+	},
+	
+	on: function(key, cb)
+	{
+		Key.keys[key] = cb;
 	}
 }
 
@@ -16,6 +28,15 @@ function handleKeys(e)
 		for ( var i in Key.ctrl )
 			if ( i == e.keyCode ) Key.ctrl[i]();
 	}
+	else if ( e.keyCode == 27)
+	{
+		Key.esc();
+	}
+	else if ( e.keyCode )
+	{
+		for ( var i in Key.keys )
+			if ( i == e.keyCode ) Key.keys[i]();
+	}
 }
 
-$(document).on('keypress', handleKeys);
+$('body').on('keyup', handleKeys);
